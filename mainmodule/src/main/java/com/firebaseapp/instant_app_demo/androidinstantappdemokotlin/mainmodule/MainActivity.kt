0c31fbc442b8c40e446db6a3b32685642b123f83
fredericcollinss <fredericcollinss@gmail.com>
2017-08-07 15:54:09 +0700

    fix issue with gradle

--- mainmodule/src/main/java/com/firebaseapp/instant_app_demo/androidinstantappdemokotlin/mainmodule/MainActivity.kt
+++ mainmodule/src/main/java/com/firebaseapp/instant_app_demo/androidinstantappdemokotlin/mainmodule/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-//import com.trustcircle.internetmodule.InternetActivity
-//import com.trustcircle.permissionmodule.PermissionActivity
+import com.trustcircle.internetmodule.InternetActivity
+import com.trustcircle.permissionmodule.PermissionActivity
 
 class MainActivity : AppCompatActivity() {
 
